2371269efede2e99f774c6c246b8d141a72fa387
dlwinn4444 <147110735+dlwinn4444@users.noreply.github.com>
2023-11-11 18:36:17 -0500

    first code complete

--- InvoiceLineItemCaculator.py
+++ InvoiceLineItemCaculator.py
@@ -1,26 +1,34 @@
-#Dwayne Winn CIS362 Invoice Line Item Caculator"
+#Dwayne Winn CIS362 Invoice Line Item Calator"
 
+#start Main function 
 def main():
+    #set grand total to 0
     g_total = float()
+    #print title
     print("The Invoice Line Item Calculator")
+    #set answer to yes
     answer = "y"
-    print(answer)
+    #start loop
     while answer == "y":
+        #get price
         price = i_price()
+        #get quantity
         quantity = i_quantity()
+        #caculate and display tota; amount
         total = price * quantity 
         print()
         print("Price: $", f"{price: .2f}") 
         print("Quantity: ", quantity)
         print("Total: $y",  f"{total: .2f}")
+        #get user input for answer
         answer = input("Enter another line item? (y/n): ")
+        #caculate grand toital
         g_total += total
-   
+    #display grand total
     print()   
     print("Grand total : $", f"{g_total: .2f}")  
     print("Thank you!") 
-   
-
+ # user input price function
 def i_price():
      while True:
          try:
@@ -28,7 +36,7 @@ def i_price():
             return price
          except ValueError:
              print("Invaild decimal.//(0.00) Please try again.")       
-         
+#user input quantity function    
 def i_quantity():
     while True:
        try:
